42b4003498bfd9b2c7091dcc7df64091072b2cd0
Мегатрон <Yan201901@yandex.ru>
2024-12-12 22:58:24 +0300

    Update MyForm.h

--- CLR1/MyForm.h
+++ CLR1/MyForm.h
@@ -9,23 +9,26 @@ namespace CLR1 {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Сводка для MyForm
+	/// РЎРІРѕРґРєР° РґР»СЏ MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 		int count = 0;
+	private: System::Windows::Forms::ProgressBar^ progressBar1;
+
+		   int count3 = 0;
 	public:
 		MyForm(void)
 		{
 			InitializeComponent();
 			//
-			//TODO: добавьте код конструктора
+			//TODO: РґРѕР±Р°РІСЊС‚Рµ РєРѕРґ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// Освободить все используемые ресурсы.
+		/// РћСЃРІРѕР±РѕРґРёС‚СЊ РІСЃРµ РёСЃРїРѕР»СЊР·СѓРµРјС‹Рµ СЂРµСЃСѓСЂСЃС‹.
 		/// </summary>
 		~MyForm()
 		{
@@ -36,25 +39,28 @@ namespace CLR1 {
 		}
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
 
 
 	protected:
 
 	private:
 		/// <summary>
-		/// Обязательная переменная конструктора.
+		/// РћР±СЏР·Р°С‚РµР»СЊРЅР°СЏ РїРµСЂРµРјРµРЅРЅР°СЏ РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР°.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Требуемый метод для поддержки конструктора — не изменяйте 
-		/// содержимое этого метода с помощью редактора кода.
+		/// РўСЂРµР±СѓРµРјС‹Р№ РјРµС‚РѕРґ РґР»СЏ РїРѕРґРґРµСЂР¶РєРё РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂР° вЂ” РЅРµ РёР·РјРµРЅСЏР№С‚Рµ 
+		/// СЃРѕРґРµСЂР¶РёРјРѕРµ СЌС‚РѕРіРѕ РјРµС‚РѕРґР° СЃ РїРѕРјРѕС‰СЊСЋ СЂРµРґР°РєС‚РѕСЂР° РєРѕРґР°.
 		/// </summary>
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -68,7 +74,7 @@ namespace CLR1 {
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(126, 17);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Счётчик кликов";
+			this->label1->Text = L"РЎС‡С‘С‚С‡РёРє РєР»РёРєРѕРІ";
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// label2
@@ -81,14 +87,38 @@ namespace CLR1 {
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(239, 17);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"Ура!Вы сделали первые 15 кликов!";
+			this->label2->Text = L"РЈСЂР°!Р’С‹ СЃРґРµР»Р°Р»Рё РїРµСЂРІС‹Рµ 15 РєР»РёРєРѕРІ!";
 			this->label2->Visible = false;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Red;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(84, 182);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(115, 17);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L" Р–РјРё РЅР° РјРµРЅСЏ!";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->label3->Click += gcnew System::EventHandler(this, &MyForm::label3_Click);
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(87, 81);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(100, 23);
+			this->progressBar1->TabIndex = 3;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(894, 476);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"MyForm";
@@ -100,17 +130,29 @@ namespace CLR1 {
 #pragma endregion
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 		count++;
-		this->label1->Text = L"Клик " + count;
+		this->label1->Text = L"РљР»РёРє " + count;
+		if (count <= 100)
+		{
+			this->progressBar1->Value = +count;
+		}
 		if (count % 10 == 0)
 		{
 			this->label2->Visible = true;
-			this->label2->Text = L"Ну наконец-то вы сделали " + count + L" кликов";
+			this->label2->Text = L"РЈСЂР°! Р’С‹ СЃРґРµР»Р°Р»Рё " + count + L" РєР»РёРєРѕРІ!";
 		}
 		else {
 			this->label2->Visible = false;
 		}
+
 	}
 	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+
+	private: System::Void label3_Click(System::Object^ sender, System::EventArgs^ e) {
+		count3++;
+		//this->label3->Text = this->label3->Text + "   ";
+		this->label3->Padding = this->label3->Padding + System::Windows::Forms::Padding(5,5,5,5);
+	}
+
+};
 }
